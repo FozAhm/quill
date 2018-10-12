4d9e57173e8a56ab8f11347da12be586eb384c89
qiqi0725 <yu.qi.liu236@gmail.com>
2018-10-12 11:18:55 -0400

    Content presentation #3
    - Smooth scrolling

--- app/client/views/home/homeCtrl.js
+++ app/client/views/home/homeCtrl.js
@@ -7,11 +7,22 @@ angular.module('reg')
     'Utils',
     'AuthService',
     function ($scope, $http, $state, settings, Utils, AuthService, $location) {
+
+      document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function (e) {
+          e.preventDefault();
+
+          document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+          });
+        });
+      });
+
       $scope.homeState = 'home';
 
       var loginStatus = false
 
-      if(sessionStorage.getItem('status') == 'login'){
+      if (sessionStorage.getItem('status') == 'login') {
         $scope.loginStatus = true;
       }
 
